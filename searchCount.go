b28b0e5f5ce9d3807db59d4086a52d0a6b2f9ceb
Louis-marie JOLY <ljoly@e2r9p5.42.fr>
2019-03-28 20:58:21 +0100

    Split searchCount and searchPopular

--- searchCount.go
+++ searchCount.go
@@ -8,12 +8,8 @@ import (
 	"time"
 )
 
-type countResponse struct {
-	Count int `json:"count"`
-}
-
-// Browse returns the Count of a Moment corresponding to a time range
-func (root *Moment) Browse(timeTokens []string) int {
+// browseTrie returns the Moment corresponding to a time range
+func (root *Moment) browseTrie(timeTokens []string) *Moment {
 
 	currentMoment := root
 	i := 0
@@ -24,37 +20,43 @@ func (root *Moment) Browse(timeTokens []string) int {
 		}
 	}
 	if i < len(timeTokens) {
-		return 0
+		return nil
 	}
-	return currentMoment.count
+	return currentMoment
+}
+
+type countResponse struct {
+	Count int `json:"count"`
 }
 
 // CountQueries returns the number of distinct queries done during a specific time range
 func (root *Moment) CountQueries(w http.ResponseWriter, r *http.Request) {
 
+	// Get time tokens
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	moment := r.URL.Path[len("/1/queries/count/"):]
-	len := len(moment)
-	if len > 19 {
+	rawMoment := r.URL.Path[len("/1/queries/count/"):]
+	if len(rawMoment) > 19 {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
-	response := countResponse{}
 	replacer := strings.NewReplacer(" ", "-", ":", "-")
-	timeTokens, _ := ParseTime(moment, replacer, false)
+	timeTokens, _ := ParseTime(rawMoment, replacer, false)
 
 	// Search
+	response := countResponse{}
 	startIndex := time.Now()
-	response.Count = root.Browse(timeTokens)
-	fmt.Printf("%-30s%s\n", moment, time.Since(startIndex))
-	if response.Count == 0 {
+	moment := root.browseTrie(timeTokens)
+	if moment == nil {
 		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
+	response.Count = moment.count
+	fmt.Printf("%-30s%s\n", rawMoment, time.Since(startIndex))
 
+	// Response
 	json, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -62,17 +64,4 @@ func (root *Moment) CountQueries(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(json)
-}
-
-type popularResponse struct {
-	Queries []Query `json:"queries"`
-}
-
-// PopularQueries returns the top popular queries done during a specific time range
-func (root *Moment) PopularQueries(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "GET" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
 }
---
+++ searchPopular.go
@@ -0,0 +1,73 @@
+package rankreq
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+// browsePopular recursively browses the tree from a particular Moment and returns top popular queries
+func (moment *Moment) browsePopular(queries []Query) {
+
+	if len(moment.children) > 0 {
+		moment.browsePopular(queries)
+	}
+	if moment.isSeconds {
+
+	}
+}
+
+type popularResponse struct {
+	Queries []Query `json:"queries"`
+}
+
+// PopularQueries returns the top popular queries done during a specific time range
+func (root *Moment) PopularQueries(w http.ResponseWriter, r *http.Request) {
+
+	// Get time tokens and size
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	rawMoment := r.URL.Path[len("/1/queries/popular/"):]
+	if len(rawMoment) > 19 {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	replacer := strings.NewReplacer(" ", "-", ":", "-")
+	timeTokens, _ := ParseTime(rawMoment, replacer, false)
+
+	q, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	size, err := strconv.ParseInt(q["size"][0], 10, 64)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	// Search
+	response := popularResponse{
+		Queries: make([]Query, size),
+	}
+	// startIndex := time.Now()
+	moment := root.browseTrie(timeTokens)
+	if moment == nil {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	moment.browsePopular(response.Queries)
+	fmt.Println(response.Queries)
+	// // Response
+	// json, err := json.Marshal(response)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write(json)
+}
