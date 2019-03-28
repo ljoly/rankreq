6e01f4aea57b44cc78ffc712eb4ae8bbc720280f
Louis-marie JOLY <ljoly@e2r9p5.42.fr>
2019-03-28 18:43:35 +0100

    Add Count response

--- search.go
+++ search.go
@@ -1,28 +1,71 @@
 package rankreq
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
 
-type countRequest struct {
+type countResponse struct {
 	Count int `json:"count"`
+}
+
+// Browse returns the Count of a Moment corresponding to a time range
+func (root *Moment) Browse(timeTokens []string) int {
+
+	currentMoment := root
+	i := 0
+	for _, timeToken := range timeTokens {
+		if found := currentMoment.children.Find(timeToken); found != nil {
+			currentMoment = found
+			i++
+		}
+	}
+	if i < len(timeTokens) {
+		return 0
+	}
+	return currentMoment.count
 }
 
 // CountQueries returns the number of distinct queries done during a specific time range
 func (root *Moment) CountQueries(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	moment := r.URL.Path[len("/1/queries/count/"):]
+	len := len(moment)
+	if len > 19 {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	response := countResponse{}
+	replacer := strings.NewReplacer(" ", "-", ":", "-")
+	timeTokens, _ := ParseTime(moment, replacer, false)
+	var err error
+
+	// Search
+	startIndex := time.Now()
+	response.Count = root.Browse(timeTokens)
+	fmt.Printf("%-30s%s\n", moment, time.Since(startIndex))
+	if response.Count == 0 {
 		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
-	reqMoment := r.URL.Path[len("/1/queries/count/"):]
-	fmt.Println(reqMoment)
-	// Parse reqMoment
 
+	json, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
-type popularRequest struct {
+type popularResponse struct {
 	Queries []Query `json:"queries"`
 }
 
